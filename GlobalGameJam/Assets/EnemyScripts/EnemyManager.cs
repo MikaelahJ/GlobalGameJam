671d6fc8d307eea195c01acb8e6d71b80afeca72
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-02-04 12:39:50 +0100

    basic enemy

---
+++ GlobalGameJam/Assets/EnemyScripts/EnemyManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    void Start()
+    {
+        //Enemy newEnemy = new Enemy(4,5,4f);
+        //EnemyOne newEnemyOne = new EnemyOne(10, 2, 3f);
+        //Instantiate(enemy);
+        //enemy.AddComponent<EnemyOne>();
+        //newEnemy.Test();
+        //newEnemyOne.Test();
+    }
+
+    void Update()
+    {
+        
+    }
+}
