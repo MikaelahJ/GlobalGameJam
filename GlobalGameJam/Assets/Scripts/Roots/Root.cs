d2a813bdbb59bcc9adff9da645e85e39014e076e
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-02-04 18:26:03 +0100

    Added roots

--- GlobalGameJam/Assets/Scripts/Roots/Root.cs
+++ GlobalGameJam/Assets/Scripts/Roots/Root.cs
@@ -8,24 +8,24 @@ public class Root : MonoBehaviour
     public Node child;
 
     public List<GameObject> subroots;
+    public List<Sprite> rootSprites;
 
     Vector2 start;
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void SpawnSubRoots(GameObject subroot, Node start, Vector2 oneSubrootDistance, int length)
     {
+        int randomRoot = 0;
+        int previousRoot = 0;
+
         for (int i = 1; i < length; i++)
         {
-            GameObject newSubRoot = Instantiate(subroot,(Vector2)start.transform.position + (oneSubrootDistance * i), Quaternion.identity, transform);
+            GameObject newSubRoot = Instantiate(subroot, (Vector2)start.transform.position + (oneSubrootDistance * i), Quaternion.identity, transform);
+            while (randomRoot == previousRoot)
+                randomRoot = Random.Range(0, rootSprites.Count);
+
+            subroot.GetComponent<SpriteRenderer>().sprite = rootSprites[randomRoot];
+            previousRoot = randomRoot;
+
             newSubRoot.name = "Subroot" + i;
             newSubRoot.transform.up = oneSubrootDistance;
             subroots.Add(newSubRoot);
